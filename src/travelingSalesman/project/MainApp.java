b3856cb74209dfc2a1130568b327e6d8eba5be03
Denis <55002785+DenisMarut@users.noreply.github.com>
2020-01-18 22:53:30 +0100

    Poprawiona ocena osobników + selekcja turniejowa

--- src/travelingSalesman/project/MainApp.java
+++ src/travelingSalesman/project/MainApp.java
@@ -2,8 +2,8 @@ package travelingSalesman.project;
 
 
 import java.io.File;
+import java.security.KeyStore;
 import java.util.*;
-
 
 
 public class MainApp {
@@ -33,7 +33,7 @@ public class MainApp {
 
             System.out.println("Size of array is equal to: " + array[0].length + " " + array[1].length);
             int a = 17;
-            int b = 15;
+            int b = 16;
             System.out.println("Element at index " + a + " " + b + " is equal to:" + array[a][b]);
             System.out.println("Element at index " + b + " " + a + " is equal to:" + array[b][a]);
 
@@ -43,7 +43,7 @@ public class MainApp {
         }
 
         int maxIndex = array[0].length;
-        //TODO
+        System.out.println(maxIndex);
         //TODO        potem sa liczone odleglosci - pomiedzy danymi indeksami 2->1->3->0 itd.
         //TODO        od 2->1 = 15, 1->3 = 45 itd. 15+45
         //TODO osobnik[2][1][3][0][5][7][4]   - nie moze sie indeks powtorzyc
@@ -52,46 +52,90 @@ public class MainApp {
 
         for (int i = 0; i < 40; i++) {
             List<Integer> sub = new ArrayList<>();
-            sub = sequence(0,51);
+            sub = sequence(0, maxIndex - 1);
             Collections.shuffle(sub);
             collectionWithSubjects.add(sub);
         }
-
-//        for (List<Integer> subjects: collectionWithSubjects
-//             ) {
-//            System.out.println(subjects);
-//        }
-
+        //TODO Ocena osobników
         List<Integer> sums = new ArrayList<>();
 
-        for (List<Integer> osobnikkk: collectionWithSubjects
-             ) {
-            int sum = 0;
-            for(int i =0; i < osobnikkk.size() - 1; i++){
-                int current = osobnikkk.get(i);
-                int next = osobnikkk.get(i + 1);
+//        for (List<Integer> subject: collectionWithSubjects
+//             ) {
+//
+//            int sum = 0;
+//            for(int i =0; i < subject.size() - 1; i++){
+//                int current = subject.get(i);
+//                int next = subject.get(i + 1);
+//
+//                sum += array[current][next];
+//            }
+//            int current = subject.get(subject.size() -1);
+//            int next = subject.get(0);
+//            sum += array[current][next];
+//            sums.add(sum);
+//            System.out.println(subject + " " + sum);
+//        }
 
-                sum += array[current][next];
+        for (List<Integer> subject : collectionWithSubjects
+        ) {
+            int judgeResult = judge(subject, array);
+            sums.add(judgeResult);
+            System.out.println(subject + " " + judgeResult);
+        }
+
+        System.out.println(sums);
+
+        //TODO Selekcja Turniejowa
+        int k = 3;
+
+        List<Integer> tournamentPopulation = new ArrayList<>();
+
+        for (int i = 0; i < 40; i++) {
+            List<Integer> integerCollection = new ArrayList<>();
+            for (int j = 0; j < k; j++) {
+                int rndRange = rndRange(collectionWithSubjects.size() - 1);
+                //z listy osobnikow wybrac tych z tymi indeksami
+                List<Integer> chosenSubject = collectionWithSubjects.get(rndRange);//mam tego osobnika
+                //pobieram jego ocene
+                Integer rating = sums.get(rndRange);
+                System.out.println("Osobnik " + chosenSubject + "ocena: " + rating);
+
+                integerCollection.add(rating);
+
             }
-            int current = osobnikkk.get(osobnikkk.size() -1);
-            int next = osobnikkk.get(0);
-            sum += array[current][next];
-
-//            osobnikkk.add(sum);
-            System.out.println(osobnikkk + " " + sum);
+            Integer min = Collections.min(integerCollection);
+            System.out.println(min);
+            tournamentPopulation.add(min);
         }
 
-        //TODO Selekcja
+        System.out.println(tournamentPopulation);
+
         //TODO Krzyzowanie i mutacja
-        
     }
-     public static List<Integer> sequence(int begin, int end){
-        List<Integer> ret = new ArrayList<>(end-begin+1);
-        for(int i = begin; i <= end; i++){
-            ret.add(i);
+
+    public static int judge(List<Integer> subject, int[][] array) {
+        int sum = 0;
+        for (int i = 0; i < subject.size() - 1; i++) {
+            int current = subject.get(i);
+            int next = subject.get(i + 1);
+            sum += array[current][next];
         }
-        return ret;
-     }
+        int current = subject.get(subject.size() - 1);
+        int next = subject.get(0);
+        sum += array[current][next];
+
+        List<Integer> result = new ArrayList<>();
+        result.add(sum);
+        return sum;
+    }
+
+    public static List<Integer> sequence(int begin, int end) {
+        List<Integer> sequenceList = new ArrayList<>(end - begin + 1);
+        for (int i = begin; i <= end; i++) {
+            sequenceList.add(i);
+        }
+        return sequenceList;
+    }
 
     public static int rndRange(int finish) {
         return new Random().nextInt(finish);
